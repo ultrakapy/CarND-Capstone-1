111cc56b88987a06f81fbebfa89c4c3e096acae1
Mark Brörkens <broerkens@gmail.com>
2018-12-01 12:22:44 +0100

    #2 DBW Node

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -33,7 +33,7 @@ class Controller(object):
         self.last_time = rospy.get_time()
 
 
-    def control(self, *args, **kwargs):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         if not dbw_enabled:
